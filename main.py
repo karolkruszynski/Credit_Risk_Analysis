8dc0a9dc047993d80a5c7f0cc105926eabdd57b5
karol <karol.kruszynski0@gmail.com>
2024-11-23 17:47:20 +0100

    find misleads value and delete from df

--- main.py
+++ main.py
@@ -34,3 +34,13 @@ print(purpose_data['purpose'].value_counts())
 print(purpose_data['credit_amount'].mean().sort_values(ascending=False))
 print(purpose_data['credit_amount'].max().sort_values(ascending=False))
 print(purpose_data['credit_amount'].min().sort_values(ascending=False))
+# We can see some issue here because purpose - other have same amount in every describe type mean, max, min
+# Iterate through groups and select groups with 'other'
+for name, group in df.groupby('purpose'):
+    if 'other' in name:
+        print(group['credit_amount'])
+# As we can see there is only 1 row of data with purpose = other with credit_amount = 18242
+# This data misleads us soo we delete this row
+df = df[~df['purpose'].str.contains('other',na=False)]
+purpose_data = df.groupby('purpose')
+print(purpose_data['purpose'].unique())
